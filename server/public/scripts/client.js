90da837019ae9dd56ecaa64784c740907fb22a1a
Stephanie Hickman <stephick@gmail.com>
2024-02-02 20:45:04 -0600

    added initial html columns, connected to database, confirmed GIT functionality is working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,42 @@
 console.log('JS is sourced!');
+
+getTodoItems();
+
+function getTodoItems(){
+    console.log( 'in getTodoItems')
+
+    //axios call to server to get To-do Items
+
+    axios({
+        method:  'GET',
+        url: 'todos'
+    }).then (response => {
+        console.log('GET response is good', response);
+        appendTodoItemsToDom(response.data);
+    }) .catch (error => {
+        console.log('GET response went wrong', error);
+    });
+
+} // end getTodoItems
+
+function appendTodoItemsToDom (todoList){
+    console.log ('in appendTodoItemsToDom function')
+    for(let todoItem of todoList){
+        let markCompleteButton = '';
+    if (!todoItem.isComplete){
+        markCompleteButton = `<button onClick="markCompleteTodoItem(${todoItem.id})" data-testid="completeButton">Mark Complete</button>`
+    } //end if statement
+    document.getElementById('viewTodoItems').innerHTML += `
+        <tr data-testid="toDoItem" data-id="${todoItem.id}">
+        <td>${todoItem.text}</td>
+        <td>${markCompleteButton}</td>
+        <td>
+        <button onClick="deleteTodoItem(${todoItem.id})" data-testid="deleteButton">Delete</button>
+        </td>
+        </tr>
+        `
+        
+
+
+
+    }}
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,5 +1,36 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
+const pg = require('pg'); // importing pg
+
+
+
+//GET
+
+router.get('/', (req, res) => {
+
+ // get To-do items from the database
+ //write a query as a string
+ 
+let queryText = `SELECT * FROM "todos";`;
+
+ //need to send the query to the db
+
+pool.query(queryText)
+    .then (
+        (result) => {
+            let todoItems = result.rows;
+
+            //Send them back to to the client
+            res.send(todoItems);
+        }
+    )
+    .catch(
+        (error) => {
+            console.log (`Error making query ${queryText}`, error);
+            res.sendStatus(500);
+        }
+    );
+});
 
 
 
