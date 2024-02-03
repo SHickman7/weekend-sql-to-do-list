77111a530fc5cf7db827062c13c9c34e201e2dd2
Stephanie Hickman <stephick@gmail.com>
2024-02-02 23:04:39 -0600

    Updated appendTodoItemsToDom function, added addToDoItem function,  and got POST to work

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -21,22 +21,50 @@ function getTodoItems(){
 
 function appendTodoItemsToDom (todoList){
     console.log ('in appendTodoItemsToDom function')
+    let todoItemsTableBody = document.getElementById('viewTodoItems');
+    todoItemsTableBody.innerHTML = ''
+    
     for(let todoItem of todoList){
-        let markCompleteButton = '';
-    if (!todoItem.isComplete){
-        markCompleteButton = `<button onClick="markCompleteTodoItem(${todoItem.id})" data-testid="completeButton">Mark Complete</button>`
-    } //end if statement
-    document.getElementById('viewTodoItems').innerHTML += `
+    //     let markCompleteButton = '';
+    // if (!todoItem.isComplete){
+    //     markCompleteButton = `<button onClick="markCompleteTodoItem(${todoItem.id})" data-testid="completeButton">Mark Complete</button>`
+    // } //end if statement
+    todoItemsTableBody.innerHTML += `
         <tr data-testid="toDoItem" data-id="${todoItem.id}">
-        <td>${todoItem.text}</td>
-        <td>${markCompleteButton}</td>
-        <td>
-        <button onClick="deleteTodoItem(${todoItem.id})" data-testid="deleteButton">Delete</button>
-        </td>
+            <td>${todoItem.text}</td>
+            <td>
+                <button onClick="markCompleteTodoItem(${todoItem.id})" data-testid="completeButton">
+                    Mark Complete
+                </button>
+            </td>
+            <td>
+                <button onClick="deleteTodoItem(${todoItem.id})" data-testid="deleteButton">Delete</button>
+            </td>
         </tr>
-        `
-        
+        `;
+       }
+    }
 
+    function addTodoItem (event){
+        event.preventDefault();
+        console.log ('in addTodoItem');
 
+        let text = document.getElementById('text').value
 
-    }}
+        let newTodoItem = { text: text}
+
+        //axios call to the server to add the to-do item
+
+        axios({
+            method: 'POST',
+            url: '/todos',
+            data: newTodoItem
+
+        }).then(response => {
+            console.log('POST request successful', response);
+            getTodoItems();
+            document.getElementById('text').value = '';
+        }).catch(error => {
+            console.log('Error in POST request', error);
+        });
+    }
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -32,7 +32,35 @@ pool.query(queryText)
     );
 });
 
+//POST
 
+router.post ('/', (req, res) => {
 
+    console.log (req.body);
+    let newTodoItem = req.body;
+
+    let queryText = `
+    INSERT INTO "todos" ("text", "isComplete")
+    VALUES
+    ($1, $2)
+    ;`;
+
+    //Need to pass the query into the pool, along with a second parameter
+    // The second parameter is the list of things we want pg to safely put into the template
+
+    pool.query(queryText, [newTodoItem.text, newTodoItem.isComplete])
+        .then(
+            (result) => {
+                console.log(`this POST query worked, ${queryText}`, result);
+                res.sendStatus(201);
+            }
+        )
+        .catch(
+            (error) => {
+                console.log (`this POST query failed, ${queryText}`, error);
+                res.sendStatus(500)
+            }
+        );
+});
 
 module.exports = router;
