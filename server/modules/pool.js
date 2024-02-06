ebe81b4776a6e8844dabf11281b0f02386a67fc5
Stephanie Hickman <stephick@gmail.com>
2024-02-05 19:42:18 -0600

    updated pool.js file

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,4 +1,5 @@
 const pg = require('pg')
+let pool;
 
 let databaseName = 'weekend-to-do-app'
 
@@ -6,11 +7,31 @@ if (process.env.NODE_ENV === 'test') {
   databaseName = 'prime_testing'
 }
 
-const pool = new pg.Pool({
-    host: 'localhost',
-    port: 5432,
-    database: databaseName,
-    allowExitOnIdle: true 
-})
+// const pool = new pg.Pool({
+//     host: 'localhost',
+//     port: 5432,
+//     database: databaseName,
+//     allowExitOnIdle: true 
+// })
+
+if (process.env.DATABASE_URL) {
+  pool = new pg.Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+          rejectUnauthorized: false
+      }
+  });
+}
+// When we're running this app on our own computer
+// we'll connect to the postgres database that is 
+// also running on our computer (localhost)
+else {
+  pool = new pg.Pool({
+      host: 'localhost',
+      port: 5432,
+      database: databaseName 
+  });
+}
+
 
 module.exports = pool
